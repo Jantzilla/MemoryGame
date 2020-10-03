c9bb299d651d51186274afbef53fb7ea23064817
Jantz <jantzillac.93@gmail.com>
2020-10-03 16:55:41 -0500

    Create action_bar and add to GameActivity

--- app/src/main/java/com/creativesource/memorygame/GameActivity.kt
+++ app/src/main/java/com/creativesource/memorygame/GameActivity.kt
@@ -2,10 +2,17 @@ package com.creativesource.memorygame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 
 class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        val actionBar: ActionBar? = this.supportActionBar
+        actionBar?.setCustomView(R.layout.action_bar)
+        actionBar?.setDisplayShowCustomEnabled(true)
+        actionBar?.setHomeAsUpIndicator(R.mipmap.ic_launcher)
+        actionBar?.show()
     }
 }
