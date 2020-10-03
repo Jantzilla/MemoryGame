f7dcdc5a2c28aae33b595c2533fca030af7ac021
Jantz <jantzillac.93@gmail.com>
2020-10-03 18:02:17 -0500

    Add CountDownTimer with auto start set to 30 seconds

--- app/src/main/java/com/creativesource/memorygame/GameActivity.kt
+++ app/src/main/java/com/creativesource/memorygame/GameActivity.kt
@@ -2,6 +2,7 @@ package com.creativesource.memorygame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.appcompat.app.ActionBar
 import kotlinx.android.synthetic.main.action_bar.*
 
@@ -18,5 +19,15 @@ class GameActivity : AppCompatActivity() {
 
         val pairs = intent.getIntExtra("Pairs", 5)
         tv_pairs.text = pairs.toString()
+
+        val timer = object: CountDownTimer(30000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                tv_time.setText((millisUntilFinished / 1000).toString())
+            }
+            override fun onFinish() {
+
+            }
+        }
+        timer.start()
     }
 }
