d63a59bd5915e5430aaa77eaff879bc7e74dc64a
Jantz <jantzillac.93@gmail.com>
2020-10-04 14:22:30 -0500

    Add extra "Rows" to intent passed to GameActivity

--- app/src/main/java/com/creativesource/memorygame/MainActivity.kt
+++ app/src/main/java/com/creativesource/memorygame/MainActivity.kt
@@ -23,21 +23,25 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         when (v?.id) {
             R.id.btn_1 -> {
                 intent.putExtra("Pairs",5)
+                intent.putExtra("Rows",5)
                 intent.putExtra("Columns",2)
                 startActivity(intent)
             }
             R.id.btn_2 -> {
                 intent.putExtra("Pairs",6)
+                intent.putExtra("Rows",4)
                 intent.putExtra("Columns",3)
                 startActivity(intent)
             }
             R.id.btn_3 -> {
                 intent.putExtra("Pairs",8)
+                intent.putExtra("Rows",4)
                 intent.putExtra("Columns",4)
                 startActivity(intent)
             }
             R.id.btn_4 -> {
                 intent.putExtra("Pairs",10)
+                intent.putExtra("Rows",5)
                 intent.putExtra("Columns",4)
                 startActivity(intent)
             }
