e6e9fd98c52d59c49eeeea5bb091f0ff23c63f4d
Jantz <jantzillac.93@gmail.com>
2020-10-03 22:52:24 -0500

    Edit CardAdapter to include new TextView - tv_card

--- app/src/main/java/com/creativesource/memorygame/CardAdapter.kt
+++ app/src/main/java/com/creativesource/memorygame/CardAdapter.kt
@@ -1,26 +1,23 @@
 package com.creativesource.memorygame
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.card.view.*
 
-class CardAdapter(private val cardCount: MutableList<Int>) :
-    RecyclerView.Adapter<CardAdapter.MyViewHolder>() {
-
-    class MyViewHolder(val imageView: ImageView) : RecyclerView.ViewHolder(imageView)
+class CardAdapter(private val cardCount: MutableList<Int>) : RecyclerView.Adapter<CardAdapter.MyViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-
-        val imageView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.card, parent, false) as ImageView
-
-        return MyViewHolder(imageView)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.card, parent, false)
+        return MyViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.imageView.tag = cardCount[position]
+        holder.itemView.tv_card.text = cardCount[position].toString()
     }
 
     override fun getItemCount() = cardCount.size
+
+    class MyViewHolder(view: View) : RecyclerView.ViewHolder(view)
 }
