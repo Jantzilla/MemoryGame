36f2c7ef6eeebe5677cf6816afd86f820e615730
Jantz <jantzillac.93@gmail.com>
2020-10-03 21:48:46 -0500

    Add and initialize RecyclerView with list of "Card Ids"

--- app/src/main/java/com/creativesource/memorygame/GameActivity.kt
+++ app/src/main/java/com/creativesource/memorygame/GameActivity.kt
@@ -5,9 +5,15 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.action_bar.*
 
 class GameActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
@@ -19,8 +25,29 @@ class GameActivity : AppCompatActivity() {
         actionBar?.show()
 
         val matched = 0
+        val columnCount = intent.getIntExtra("Columns", 2)
         val pairs = intent.getIntExtra("Pairs", 5)
+
         tv_pairs.text = pairs.toString()
+
+        val cardIds: MutableList<Int> = ArrayList()
+
+        for (i in 1..pairs) {
+            cardIds.add(i)
+            cardIds.add(i)
+        }
+
+        cardIds.shuffle()
+
+        viewManager = GridLayoutManager(this, columnCount)
+        viewAdapter = CardAdapter(cardIds)
+
+        recyclerView = findViewById<RecyclerView>(R.id.recyclerView).apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+
+        }
 
         val timer = object: CountDownTimer(30000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
